d95f0609f0ec28868916d3dbaf522fdb719144d9
Shreejeet-07 <shreejeetpatnaik07@gmail.com>
2026-04-01 21:52:02 +0530

    Fix caching - unregister service workers and add no-cache headers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './App.css';
 import App from './App';
 import { ThemeProvider } from './components/ThemeContext';
 
+// Unregister any old service workers to prevent caching issues
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider>
